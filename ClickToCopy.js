4a94554d7de95d47eba92313826aeb031c7ad36b
Mike Heggie <mike@heggie.design>
2025-06-03 22:34:37 -0600

    removed domcontentloaded to start using defer only

--- ClickToCopy.js
+++ ClickToCopy.js
@@ -1,55 +1,53 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const copyButtons = document.querySelectorAll('[data-copy="button"]');
+const copyButtons = document.querySelectorAll('[data-copy="button"]');
 
-	copyButtons.forEach((copyButton) => {
-		copyButton.addEventListener("click", function () {
-			const buttonText = this.querySelector('[data-copy="button-text"]');
-			const jsonEl = this.querySelector('[data-copy="json"]');
-			const txtEl = this.querySelector('[data-copy="txt"]');
-			if (!buttonText) return;
+copyButtons.forEach((copyButton) => {
+	copyButton.addEventListener("click", function () {
+		const buttonText = this.querySelector('[data-copy="button-text"]');
+		const jsonEl = this.querySelector('[data-copy="json"]');
+		const txtEl = this.querySelector('[data-copy="txt"]');
+		if (!buttonText) return;
 
-			const originalText = buttonText.textContent;
-			buttonText.textContent = "Copying...";
+		const originalText = buttonText.textContent;
+		buttonText.textContent = "Copying...";
 
-			if (jsonEl) {
-				const componentJson = jsonEl.textContent;
+		if (jsonEl) {
+			const componentJson = jsonEl.textContent;
 
-				const copyJson = (event) => {
-					event.preventDefault();
-					event.clipboardData.setData("application/json", componentJson);
-				};
+			const copyJson = (event) => {
+				event.preventDefault();
+				event.clipboardData.setData("application/json", componentJson);
+			};
 
-				document.addEventListener("copy", copyJson);
-				document.execCommand("copy");
-				document.removeEventListener("copy", copyJson);
+			document.addEventListener("copy", copyJson);
+			document.execCommand("copy");
+			document.removeEventListener("copy", copyJson);
 
+			setTimeout(() => {
+				buttonText.textContent = "Copied!";
 				setTimeout(() => {
-					buttonText.textContent = "Copied!";
-					setTimeout(() => {
-						buttonText.textContent = originalText;
-					}, 1000);
+					buttonText.textContent = originalText;
 				}, 1000);
-			} else if (txtEl) {
-				const txtValue = txtEl.textContent;
-				navigator.clipboard
-					.writeText(txtValue)
-					.then(() => {
-						setTimeout(() => {
-							buttonText.textContent = "Copied!";
-							setTimeout(() => {
-								buttonText.textContent = originalText;
-							}, 1000);
-						}, 1000);
-					})
-					.catch(() => {
-						buttonText.textContent = "Failed to copy";
+			}, 1000);
+		} else if (txtEl) {
+			const txtValue = txtEl.textContent;
+			navigator.clipboard
+				.writeText(txtValue)
+				.then(() => {
+					setTimeout(() => {
+						buttonText.textContent = "Copied!";
 						setTimeout(() => {
 							buttonText.textContent = originalText;
 						}, 1000);
-					});
-			} else {
-				buttonText.textContent = originalText;
-			}
-		});
+					}, 1000);
+				})
+				.catch(() => {
+					buttonText.textContent = "Failed to copy";
+					setTimeout(() => {
+						buttonText.textContent = originalText;
+					}, 1000);
+				});
+		} else {
+			buttonText.textContent = originalText;
+		}
 	});
 });
